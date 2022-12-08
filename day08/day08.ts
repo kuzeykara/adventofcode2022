411051fb425df3150dd00378d2de1a7b1e8a7349
Kuzey Kara <kuzeykara07@gmail.com>
2022-12-08 12:01:10 +0100

    solved part 1 of day 8

---
+++ day08/day08.ts
@@ -0,0 +1,105 @@
+import { readFileSync } from "fs";
+
+const inputFile = 'input.txt';
+const input: string = readFileSync(inputFile, 'utf-8').replace(/\r/g, '');
+
+function part01() {
+    function parseTreesInput(input: string): number[][] {
+        let treesGrid: number[][] = input.split('\n').map( (line: string) => {
+            let treeLine: number[] = [];
+            for (let i=0; i<line.length; i++) {
+                let currChar = line.charAt(i);
+                treeLine.push(Number(currChar));
+            }
+            return treeLine;
+        } );
+
+        return treesGrid;
+    }
+
+    function isVisibleFromDirection(treeHeight: number, treesCovering: number[]): boolean {
+        for (let i=0; i<treesCovering.length; i++) {
+            if (treesCovering[i] >= treeHeight) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function isTreeVisible(treePos: number[], grid: number[][]): boolean {
+        let leftSide: number[] = [];
+        let rightSide: number[] = [];
+        let topSide: number[] = [];
+        let bottomSide: number[] = [];
+        let treeHeight: number = 0;
+
+        // establish the sides
+        for (let y=0; y<grid.length; y++) {
+            for (let x=0; x<grid[y].length; x++) {
+                if (x == treePos[0] && y == treePos[1]) {
+                    // ourNumber
+                    treeHeight = grid[y][x];
+                } else if (y == treePos[1]) {
+                    // same y
+                    if (x < treePos[0]) {
+                        leftSide.push(grid[y][x]);
+                    } else {
+                        rightSide.push(grid[y][x]);
+                    }
+                } else if (x == treePos[0]) {
+                    // same x
+                    if (y < treePos[1]) {
+                        topSide.push(grid[y][x]);
+                    } else {
+                        bottomSide.push(grid[y][x]);
+                    }
+                }
+            }
+        }
+
+        // check the heights from each side
+        if (
+            isVisibleFromDirection(treeHeight, leftSide) ||
+            isVisibleFromDirection(treeHeight, rightSide) ||
+            isVisibleFromDirection(treeHeight, topSide) ||
+            isVisibleFromDirection(treeHeight, bottomSide)
+        ) {
+            return true;
+        }
+        
+        /*
+        console.log(`Left side: ${leftSide}`);
+        console.log(`Right side: ${rightSide}`);
+        console.log(`Top side: ${topSide}`);
+        console.log(`Bottom side: ${bottomSide}`);
+        */
+
+        return false;
+    }
+
+    function visibleTreesCount(treeGrid: number[][]): number {
+        let visibleTreeCount: number = 0;
+
+        for (let y=0; y<treeGrid.length; y++) {
+            for (let x=0; x<treeGrid[y].length; x++) {
+                if (isTreeVisible([x, y], treeGrid)) {
+                    visibleTreeCount++;
+                }
+            }
+        }
+
+        return visibleTreeCount;
+    }
+
+    const treeGrid: number[][] = parseTreesInput(input);
+    const visibleTrees: number = visibleTreesCount(treeGrid);
+
+    console.log(treeGrid);
+}
+
+function part02() {
+    
+}
+
+part01();
+//part02();
