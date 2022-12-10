a7ad2f3e8591df3e8f7e85856bac0c70bf68605e
Kuzey Kara <kuzeykara07@gmail.com>
2022-12-10 18:19:15 +0100

    initialized day 10

---
+++ day10/day10.ts
@@ -0,0 +1,15 @@
+import { readFileSync } from "fs";
+
+const inputFile = 'input.txt';
+const input: string = readFileSync(inputFile, 'utf-8').replace(/\r/g, '');
+
+function part01() {
+    
+}
+
+function part02() {
+    
+}
+
+part01();
+//part02();
