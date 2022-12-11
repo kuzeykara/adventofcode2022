0c29acd55e34aa42789fcab0a7bc5b4497e4f10e
Kuzey Kara <kuzeykara07@gmail.com>
2022-12-11 01:37:00 +0100

    solved part 2

--- day10/day10.ts
+++ day10/day10.ts
@@ -57,8 +57,83 @@ function part01() {
 }
 
 function part02() {
-    
+    function addX(x: number, value: number): number {
+        return x+value;
+    }
+
+    function drawOnCRT(currCRTRow: string[], CRTIndex: number, x: number): string[] {
+        let newCRTRow: string[] = currCRTRow;
+
+        if (x == CRTIndex || x-1 == CRTIndex || x+1 == CRTIndex) {
+            newCRTRow.push('#');
+        } else {
+            newCRTRow.push('.');
+        }
+
+        return newCRTRow;
+    }
+
+    function execute(instructions: string[][]): string[][] {
+        let CRTScreen: string[][] = [[], [], [], [], [], []];
+        let CRTIndex: number = 0;
+        let CRTHeight: number = 0;
+        let x: number = 1;
+        let instructionIndex: number = 0;
+        // busy=true -> addX called last cycle
+        let busy: boolean = false;
+
+        for (let cycle=1; cycle<241; cycle++) {
+            CRTScreen[CRTHeight] = drawOnCRT(CRTScreen[CRTHeight], CRTIndex, x);
+            
+            let currInstruction: string[] = instructions[instructionIndex];
+            if (!busy) {
+                if (currInstruction[0] === 'noop') {
+                    // do nothing
+                    instructionIndex += 1;
+                } else {
+                    busy = true;
+                }
+            } else {
+                // finishing executing
+                x = addX(x, Number(currInstruction[1]));
+                instructionIndex += 1;
+                busy = false;
+            }
+            
+            if (cycle%40==0) {
+                CRTHeight += 1;
+                CRTIndex = 0;
+            } else {
+                CRTIndex += 1;
+            }
+        }
+
+        return CRTScreen;
+    }
+
+    function displayScreen(CRT: string[][]): void {
+        let screenStr: string = '';
+        
+        for (let i=0; i<CRT.length; i++) {
+            let currRow: string[] = CRT[i];
+            for (let j=0; j<currRow.length; j++) {
+                screenStr += currRow[j];
+            }
+            screenStr += '\n';
+        }
+
+        console.log(screenStr);
+    }
+
+    const inputInstructions: string[][] = input
+    .split('\n')
+    .map( (instr: string) => {
+        return instr.split(' ');
+    } );
+    const screen: string[][] = execute(inputInstructions);
+
+    displayScreen(screen);
 }
 
-part01();
-//part02();
+//part01();
+part02();
